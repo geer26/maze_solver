6354e41f9de9c2f0ef6721e4a1b9aeec4685660c
geer26 <gergo.kurucz@gmail.com>
2022-02-17 14:32:26 +0100

    V0.1

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class Unit():
         self.id = uuid.uuid4()
         self.visited = False
         self.level = level
-        print(self.id)
+
 
 
 class Maze():
@@ -26,30 +26,68 @@ class Maze():
     height = None
     width = None
     image = None
+    doors = None
     level = 0
     map = []
     tree = []
-
 
     def __init__(self, filename = "maze.bmp"):
         self.image = self.convert_color(Image.open(filename))
         self.width = self.image.size[0]
         self.height = self.image.size[1]
 
+        self.genmap(self.image)
+        self.doors = self.get_doors()
+        return None
+
     def convert_color(self, image):
         return ImageOps.grayscale(image)
 
     def genmap(self, image):
-        for x in range(self.width):
-            for y in range(self.height):
-                #print(image.getpixel((x,y)))
-                if image.getpixel((x,y)) <= 127:
-                    newpixel = Unit(x, x, True, self.level)
+        for y in range(self.width):
+            for x in range(self.height):
+                if image.getpixel((x,y)) < 128:
+                    newpixel = Unit(x, y, True, self.level)
                     self.map.append(newpixel)
-                if y == self.height-1: print('\n')
+                else:
+                    newpixel = Unit(x, y)
+                    self.map.append(newpixel)
+        return None
+
+    def get_doors(self):
+        doors = []
+        for point in self.map:
+            if point.y_pos == 0:
+                if not point.wall:
+                    doors.append(point)
+                    continue
+            if point.y_pos == self.height-1:
+                if not point.wall:
+                    doors.append(point)
+                    continue
+            if point.x_pos == 0:
+                if not point.wall:
+                    doors.append(point)
+                    continue
+            if point.x_pos == self.width-1:
+                if not point.wall:
+                    doors.append(point)
+                    continue
+        #for door in doors:
+        #    print(f'{door.id} :: {door.x_pos}:{door.y_pos} - {door.wall}')
+        return doors
+
+    def solve(self):
+        actual_point = self.doors[0]
+        actual_point.level = 0
+        actual_point.visited = True
+        while actual_point != self.doors[1]:
+
+            pass
+
 
 
 
 if __name__ == "__main__":
     im = Maze()
-    im.genmap(im.image)
+    im.solve()
