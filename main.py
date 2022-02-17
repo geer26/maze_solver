6178f8333bd24691895e9cc818b2e8472513ec17
geer26 <gergo.kurucz@gmail.com>
2022-02-17 10:51:54 +0100

    V0.1

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 from PIL import Image, ImageOps
+import uuid
+
+class Unit():
+
+    x_pos = None
+    y_pos = None
+    visited = False
+    wall = False
+    level = None
+    id = None
+    parent = None
+
+    def __init__(self, xpos = None, ypos = None, wall = False, level = None):
+        self.x_pos = xpos
+        self.y_pos = ypos
+        self.wall = wall
+        self.id = uuid.uuid4()
+        self.visited = False
+        self.level = level
+        print(self.id)
+
 
 class Maze():
 
     height = None
     width = None
     image = None
+    level = 0
     map = []
+    tree = []
 
 
     def __init__(self, filename = "maze.bmp"):
@@ -13,27 +36,20 @@ class Maze():
         self.width = self.image.size[0]
         self.height = self.image.size[1]
 
-        print(self.image.format, self.image.size, self.image.mode)
-
     def convert_color(self, image):
         return ImageOps.grayscale(image)
 
     def genmap(self, image):
         for x in range(self.width):
             for y in range(self.height):
-                print(image.getpixel((x,y)))
+                #print(image.getpixel((x,y)))
+                if image.getpixel((x,y)) <= 127:
+                    newpixel = Unit(x, x, True, self.level)
+                    self.map.append(newpixel)
                 if y == self.height-1: print('\n')
 
 
 
-def get_image(name = "maze.bmp"):
-    im = Image.open(name)
-
-    print(im.format, im.size, im.mode)
-
-
 if __name__ == "__main__":
-    print("Hello world!")
-    #get_image()
     im = Maze()
     im.genmap(im.image)
